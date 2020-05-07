f362d3a6dcfc1d4faabd263dd1d115ce2d6093c8
tigran-avdalyan <tigran.avdalyan@hotmail.com>
2020-05-07 11:13:13 +0400

    Add files via upload

---
+++ js/hy_HY.js
@@ -0,0 +1,18 @@
+// Russian
+
+jQuery.extend( jQuery.fn.pickadate.defaults, {
+    monthsFull: [ 'հունվար', 'փետրվար', 'մարտ', 'ապրիլ', 'մայիս', 'հունիս', 'հուլիս', 'օգոստոս', 'սեպտեմբեր', 'հոկտեմբեր', 'նոյեմբեր', 'դեկտեմբեր' ],
+    monthsShort: [ 'янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек' ],
+    weekdaysFull: [ 'կիրակի', 'երկուշաբթի', 'երեքշաբթի', 'չորեքշաբթի', 'հինգշաբթի', 'ուրբաթ', 'շաբաթ' ],
+    weekdaysShort: [ 'կիր', 'երկ', 'երե', 'չոր', 'հին', 'ուրբ', 'շաբ' ],
+    today: 'այսօր',
+    clear: 'ջնջել',
+    close: 'փակել',
+    firstDay: 1,
+    format: 'd mmmm yyyy թ.',
+    formatSubmit: 'yyyy/mm/dd'
+});
+
+jQuery.extend( jQuery.fn.pickatime.defaults, {
+    clear: 'ջնջել'
+});
